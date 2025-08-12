f7163cafd06055ccb4263749fa7045b76089c8a1
educd2017 <44037870+educd2017@users.noreply.github.com>
2025-08-12 07:57:17 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,11 @@ self.addEventListener('install', e => {
     caches.open('printast-pwa-v1').then(cache => {
       return cache.addAll([
         '/',
+        '/p/products.html',
+        '/2025/08/eltakhrg.html',
+        '/p/client-review.html',
+        '/p/about.html',
+        '/p/faq.html',
         '/index.html'
       ]);
     })
